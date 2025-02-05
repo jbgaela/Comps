39a718eb3aa7c12657e6e220d41f492343bb387d
jbgaela <jbgaela@gmail.com>
2025-02-06 06:19:47 +0800

    Added tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Button from "./Button";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Button success primary>
+          Click me!
+        </Button>
+      </div>
+      <div>
+        <Button>Buy now!</Button>
+      </div>
+      <div>
+        <Button>See Deal</Button>
+      </div>
+      <div>
+        <Button>asdasd</Button>
+      </div>
+      <div>
+        <Button>asdasd</Button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
